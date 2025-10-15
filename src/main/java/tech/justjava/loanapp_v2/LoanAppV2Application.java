a1b8b53d5b10e664192e4e49f63614ddbbe664d7
Just Java Consultancy <kazeem.akinrinde72@gmail.com>
2025-10-15 14:58:24 +0100

    Initial commit: Spring Boot starter auto-generated by Agentic AI

---
+++ src/main/java/tech/justjava/loanapp_v2/LoanAppV2Application.java
@@ -0,0 +1,13 @@
+package tech.justjava.loanapp_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanAppV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanAppV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/tech/justjava/loanapp_v2/LoanAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.justjava.loanapp_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
